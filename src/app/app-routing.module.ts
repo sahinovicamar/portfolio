e93ea17f6a3b6f0b5ba512c6ec3f2dca9f56f852
Amar Sahinovic <101928597+sahinovicamar@users.noreply.github.com>
2023-02-25 23:34:07 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { HeaderComponent } from './header/header.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: HeaderComponent },
+  { path: '', component: StartPageComponent },
+  { path: '', component: AboutMeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StartPageComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
