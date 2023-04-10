3ce19e10a6868cc06a4dbe8b1a8086faa83fba22
Amar Sahinovic <101928597+sahinovicamar@users.noreply.github.com>
2023-04-10 16:50:42 +0200

    skills portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { StartPageComponent } from './start-page/start-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+// import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   // { path: '', component: HeaderComponent },
   { path: '', component: StartPageComponent },
   { path: '', component: AboutMeComponent },
   { path: '', component: MySkillsComponent },
-  { path: '', component: ProjectsComponent }
+  { path: '', component: ProjectsComponent },
+  { path: '', component: ContactComponent }
+  // { path: '', component: FooterComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StartPageComponent } from './start-page/start-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { ProjectsComponent } from './projects/projects.component';
     StartPageComponent,
     AboutMeComponent,
     MySkillsComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
